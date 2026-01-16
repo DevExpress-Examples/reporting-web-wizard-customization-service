86b4ea1e167391a11262c0b3f95d44422ca35850
Polina T <56310766+pollyndos@users.noreply.github.com>
2026-01-16 19:25:05 +0400

    Update Startup.cs

--- AspNetCore/RWCSAspNetCoreExample/Startup.cs
+++ AspNetCore/RWCSAspNetCoreExample/Startup.cs
@@ -51,7 +51,7 @@ namespace ReportWizardCustomizationServiceAspNetCoreExample {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, ReportDbContext db) {
             db.InitializeDatabase();
             var contentDirectoryAllowRule = DirectoryAccessRule.Allow(new DirectoryInfo(Path.Combine(env.ContentRootPath, "..", "Content")).FullName);
-            AccessSettings.ReportingSpecificResources.TrySetRules(contentDirectoryAllowRule, UrlAccessRule.Allow());
+            AccessSettings.ReportingSpecificResources.SetRules(contentDirectoryAllowRule, UrlAccessRule.Deny());
             DevExpress.XtraReports.Configuration.Settings.Default.UserDesignerOptions.DataBindingMode = DevExpress.XtraReports.UI.DataBindingMode.Expressions;
             app.UseDevExpressControls();
             System.Net.ServicePointManager.SecurityProtocol |= System.Net.SecurityProtocolType.Tls12;
@@ -76,3 +76,4 @@ namespace ReportWizardCustomizationServiceAspNetCoreExample {
         }
     }
 }
+
